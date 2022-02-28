34ba13645a6183af95a13d68b62f759a5ec2cd62
ashustha <ashu.stha08@gmail.com>
2022-02-28 16:32:03 +0545

    navbar

--- dev/scripts/modules/app.js
+++ dev/scripts/modules/app.js
@@ -2,6 +2,7 @@ import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/tab';
 import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/collapse';
 class global {
     constructor() {
         this.events();
